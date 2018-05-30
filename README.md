a9d6dad66eab8378f0ad0695b51a772ace87c7a4
Edward <edwardgretsch@gmail.com>
2018-05-29 22:05:32 -0700

    update2

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # triviaGame
 https://egretsch.github.io/triviaGame/
+
+####model
+        questions
+        timer 
+        congradulate forr right answers
+        keep track of anwsers
+            correct answers, 
+            incorrect answers
+        
+            
+####view
+        webpage 
+            title
+            body
+            display one question
+            display timer
+            backgorund
+            congradulate forr right answers
+        
+
+
+####controler
+        when page changes it updates display
+        when incorret answer 
+            loged
+            timer hits 0 
+            or selected worng answer
+            show correct answer
+        log correct answer
+        congradulate forr right answers
+        option to restart the game
+click event
+    click on answer 
+    keep track of 
+        right 
+        or wrong answer
+        
+
+        
---
+++ assets/javascript/app.js
@@ -0,0 +1,93 @@
+var correct = 0;
+var incorrect = 0;
+var unAnwered = 0;
+var counter;
+var time = 5;
+var congarts = ("Congragulations!!!")
+var index = 0;
+
+var sifyQuestion = [{
+        question: "Witch on is the Captin from star trek the next generation?",
+        choices: ["Picard", "Kirk", "Hook", "Jack"],
+        validAnswer: 0
+    }, {
+        question: "With disney scifi movie is not well know?",
+        choices: ["Tron", "The Black Hole", "Tomorrowland", "Right Hand"],
+        validAnswer: 1
+    }, {
+        question: "Witch is a a sify tv show with 1 season?",
+        choices: ["X-files", "The Black Hole", "Fierfly", "Stargate SG-1"],
+        validAnswer: 3
+    }, {
+        question: "Who was the directer of buffy the vampire slayer and angle?",
+        choices: ["Joss whedon", "Clint Eastwood", "Steven Spielberg", "Martin Scorsese"],
+        validAnswer: 0
+    }, {
+        question: "Who has not directed star wars?",
+        choices: ["George Lucas", "Rian Johnson", "Andy Serkis", "Christopher Nolan"],
+        validAnswer: 2
+    }, {
+        question: "Wtich is the longist runing sify show?",
+        choices: ["X - files", "Dr. Who", "Star Trek", "Stargate SG-1"],
+        validAnswer: 1
+    }, {
+        question: "Witch Studio Created the movie Spirted Away?",
+        choices: ["Disney", "Studio Bones", "Mad house", "Studio Ghibli"],
+        validAnswer: 3
+    }, {
+        question: "Witch is not a large robot anime?",
+        choices: ["Gundem Wing", "Rusty The Boy Robot", "Gigantor", "Voltron"],
+        validAnswer: 1
+    },  
+
+];
+
+function timer(time) {
+    counter = setTimeout(function (){
+        console.log(time);
+
+        time--;
+
+        if(time > 0){
+            timer(time);
+        }else{
+            console.log("next question")
+
+            index++
+
+            buildQuestionsDiv(sifyQuestion, index);
+            timer(5);
+        }
+
+        
+    }, 1000);
+}
+
+function answerGongrats(message) {
+    setTimeout(function () {
+        console.log(message)
+    },5000)
+
+    
+}
+
+function buildQuestionsDiv(array, index) {
+    $("#question").empty();
+    var qustionDiv = $("<div>");
+    // setup div to display
+    qustionDiv.text(
+            array[index].question
+    )
+    $("#question").append(qustionDiv)
+    
+};
+
+answerGongrats(sifyQuestion[0].choices[sifyQuestion[0].validAnswer]);
+ timer(5)
+function nextQuestion(){
+
+}
+ function resetGame(params) {
+     
+ }
+
